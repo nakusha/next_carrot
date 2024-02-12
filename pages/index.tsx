133e62ecc9f19ac9debbec19ddbc2eb236eaf54f
YeonsuJeong <runnway@naver.com>
2024-02-12 18:14:34 +0900

    test tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,5 +3,9 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <></>;
+  return (
+    <div className="bg-red-100">
+      <h1 className="text-blue-500">it works</h1>
+    </div>
+  );
 }
